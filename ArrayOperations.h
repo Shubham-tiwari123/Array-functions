9bfebc7e24620234facb16beb1c8e4e339433999
Shubham-tiwari123 <st540362@gmail.com>
2018-03-25 19:37:44 +0530

    class file

---
+++ ArrayOperations.h
@@ -0,0 +1,17 @@
+#ifndef ARRAYOPERATIONS_H
+#define ARRAYOPERATIONS_H
+
+class ArrayOperations{
+private:
+    int num,element,temp,j,i,num1,key,mid;
+    int A[10];
+public:
+    void acceptData();
+    void diplayData();
+    void arrayRotate();
+    void arrayCylicRotation();
+    void searchElement();
+};
+
+#endif /* ARRAYOPERATIONS_H */
+
