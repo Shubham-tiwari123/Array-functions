ebaf96d9ad9d38aadce2a4a957d444a92586a3a9
Shubham-tiwari123 <st540362@gmail.com>
2018-03-25 19:35:22 +0530

    main file

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include "ArrayOperations.h"
+using namespace std;
+int main() {
+    ArrayOperations a;
+    a.acceptData();
+    a.diplayData();
+    a.arrayRotate();
+    a.diplayData();
+   // a.arrayCylicRotation();
+   // a.diplayData();
+    a.searchElement();
+    return 0;
+}
+
