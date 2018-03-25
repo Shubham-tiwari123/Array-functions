b2597ea84373bead2f67118bbc998ccd32796d96
Shubham-tiwari123 <st540362@gmail.com>
2018-03-25 22:05:57 +0530

    class file

--- ArrayOperations.h
+++ ArrayOperations.h
@@ -4,6 +4,8 @@
 class ArrayOperations{
 private:
     int num,element,temp,j,i,num1,key,mid;
+    int sum=0;
+    int flag =0;
     int A[10];
 public:
     void acceptData();
@@ -11,6 +13,7 @@ public:
     void arrayRotate();
     void arrayCylicRotation();
     void searchElement();
+    void addElement();
 };
 
 #endif /* ARRAYOPERATIONS_H */
